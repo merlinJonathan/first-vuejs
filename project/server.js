e43bd33f71e7f340c14c173083cd80ae106098b4
merlinJonathan <jo_merlin@hotmail.fr>
2022-01-08 14:48:34 +0100

    init

--- project/server.js
+++ project/server.js
@@ -1,8 +1,7 @@
 const express = require('express');
+const serveStatic = require("serve-static")
 const path = require('path');
-const app = express();
-app.use(express.static(__dirname + '/dist/firsttime'));
-app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+
-'/dist/firsttime/index.html'));});
-app.listen(process.env.PORT || 8080);
+app = express();
+app.use(serveStatic(path.join(__dirname, 'dist')));
+const port = process.env.PORT || 3000;
+app.listen(port);
