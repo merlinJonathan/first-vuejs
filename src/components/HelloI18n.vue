8587fc90c5f52470931f57ff0344c862eb9f90cb
Jonathan Foucher <jonathan-foucher@outlook.fr>
2022-01-09 17:06:04 +0100

    add vue-country-flag-next to get the country flags

--- src/components/HelloI18n.vue
+++ src/components/HelloI18n.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <country-flag :country="getCountry" size='normal'/>
     <div class="locale-changer">
       <select v-model="$i18n.locale">
         <option v-for="(lang, i) in $i18n.availableLocales" :key="`Lang${i}`" :value="lang">
@@ -16,6 +17,12 @@ import { defineComponent } from 'vue'
 
 export default defineComponent({
   name: 'HelloI18n',
+
+  computed: {
+    getCountry() {
+      return this.$i18n.locale === 'en' ? 'gb' : this.$i18n.locale;
+    }
+  }
 })
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import i18n from './i18n'
+import CountryFlag from 'vue-country-flag-next'
 
-createApp(App).use(i18n).mount('#app')
+createApp(App)
+  .use(i18n)
+  .component('country-flag', CountryFlag)
+  .mount('#app');
